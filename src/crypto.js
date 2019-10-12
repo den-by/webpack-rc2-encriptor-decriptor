8789ce430cedbb27822d30839ab2529349059d05
Denis <denis.da.by@gmail.com>
2019-10-12 20:17:28 +0300

    Смог включить rc2.js в бандл

--- src/crypto.js
+++ src/crypto.js
@@ -1,13 +1,20 @@
-// import CryptoJS from './rc2'
+import CryptoJSImport from './rc2'
 export default class Crypto {
 
+
     static encryptRC2(data, password) {
-         console.log(CryptoJS);
+
+        let CryptoJS = CryptoJSImport();
+        // CryptoJS();
+        //  console.log(CryptoJS);
+         // debugger
         const cipherText2 = CryptoJS.RC2.encrypt(data, password);
         return cipherText2
         // return '1'
     }
     static decryptRC2(data, password) {
+        debugger
+        let CryptoJS=CryptoJSImport();
         let bytes = CryptoJS.RC2.decrypt(data, password);
         let plaintext = bytes.toString(CryptoJS.enc.Utf8);
         return plaintext;
